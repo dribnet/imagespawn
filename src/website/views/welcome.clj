e76fb2afdababc38b1c332363c5004f262385cc9
Tom White <tom@sixdozen.com>
2011-12-11 12:54:38 -0800

    Added default noir 1.2.1 website.
    As a starting point, imply the combined result of:
    
    lein plugin install lein-noir 1.2.1
    lein noir new website

---
+++ src/website/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns website.views.welcome
+  (:require [website.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to website"]))
